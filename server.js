9daf1e929f6a605b926292ac8f36dab6291e9425
anya.shan <anya.shanaida@yahoo.com>
2023-01-26 14:04:30 +0200

    add .env

--- server.js
+++ server.js
@@ -1,5 +1,26 @@
-const app = require('./app')
+const mongoose = require("mongoose");
+const app = require("./app");
 
-app.listen(3000, () => {
-  console.log("Server running. Use our API on port: 3000")
-})
+const { PORT = 3000, DB_HOST } = process.env;
+
+const connection = mongoose.connect(DB_HOST, {
+  promiseLibrary: global.Promise,
+  useCreateIndex: true,
+  useUnifiedTopology: true,
+  useFindAndModify: false,
+});
+
+connection
+  .then(() => {
+    app.listen(PORT, function () {
+      console.log(`Database connection successful`);
+    });
+  })
+  .catch((error) => {
+    console.log(error.message);
+    process.exit(1);
+  });
+
+// app.listen(3000, () => {
+//   console.log("Server running. Use our API on port: 3000");
+// });
