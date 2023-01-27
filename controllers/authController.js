759aaf34c012fd0817ffa1c0b05058ef72dc614e
anya.shan <anya.shanaida@yahoo.com>
2023-01-27 18:34:04 +0200

    add bcryptjs

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,14 +1,17 @@
+const dcrypt = require("bcryptjs");
 const { controllerWrapper, HttpError } = require("../helpers/index");
 const { User } = require("../models/users");
 
 const register = async (req, res) => {
-  const { email } = req.body;
+  const { email, password } = req.body;
   const user = await User.findOne({ email });
   if (user) {
     throw HttpError(409, "Email in use");
   }
 
-  const newUser = await User.create(req.body);
+  const hashPassword = await dcrypt.hash(password, 10);
+
+  const newUser = await User.create({ ...req.body, password: hashPassword });
 
   res.status(201).json({
     status: "success",
